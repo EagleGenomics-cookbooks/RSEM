1bf10878379fa40f7e2ff3be8ae7070f956df806
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-11-02 10:54:13 +0000

    Fixed updated rubocop issue

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 # 'master' can be used for the current version
-default['RSEM']['version'] =  'v1.2.20'
+default['RSEM']['version'] = 'v1.2.20'
 default['RSEM']['install'] = '/usr/local'
 default['RSEM']['dir'] = default['RSEM']['install'] + '/' + 'RSEM_' + default['RSEM']['version']
